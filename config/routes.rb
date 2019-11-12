66e3e2b9e0a7498d9500320e9f3a67423faf3703
Leo <ronald.mauritsson@gmail.com>
2019-11-12 21:42:45 +0300

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'users/new'
-  get 'static_pages/help', to: 'static_pages#help'
-  get 'static_pages/about', to: 'static_pages#about'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
   get '/signup',  to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'static_pages#home'
